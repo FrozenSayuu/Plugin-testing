78bf2f04de3e000ede852b5d882255227551a172
Tova0811 <91182586+Tova0811@users.noreply.github.com>
2022-06-05 17:36:29 +0200

    added shortcode for comment plugin

--- testtheme/index.php
+++ testtheme/index.php
@@ -1,5 +1,10 @@
 <h1>test</h1>
 
 <?php
+ini_set ('display_errors', 1);
+ini_set ('display_startup_errors', 1);
+error_reporting (E_ALL);
+
 echo do_shortcode('[search_bar_hello]');
+echo do_shortcode('[comment_hello]');
 ?>
