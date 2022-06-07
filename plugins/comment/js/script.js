53dfc5552dd3b2ba82654fed1d6c1c604fe9b31a
Tova0811 <tova.folkesson@gmail.com>
2022-06-07 10:31:39 +0200

    working comment plugin with ajax form

--- plugins/comment/js/script.js
+++ plugins/comment/js/script.js
@@ -1,26 +1,19 @@
-
-/**
- * Hanterar exempel 2 formuläret och skicka formuläret med fetch
- */
-
 let saveComment = document.querySelector('#commentform');
-saveComment.addEventListener('submit', doAjax);   // Lyssna på submit på formuläret, kör doAjax2 när det submittas.
+saveComment.addEventListener('submit', doAjax);   // Listenes at submit on the form, runs doAjax function when activated.a
 
 function doAjax(event)
 {
     event.preventDefault();
 
     /*
-     * Skapa formulär data som skickas med fetch request:et.
-     * Det gör vi igenom JS klassen Formdata, som tar emot ett formulär som input.
-     * De kommer den lägga ni alla våra inputs med name, som sedan kan skickas som form post data
-     *
-     * I exemplet ovan la vi till action med appendd(), men nu finns den som en hidden input.
+     * Create form data that sends with fetch request.
+     * This we do with the JS class Formdata, that takes it as an input.
+     * It will put in all of our inputs with name, that can later send as form post data.
      * */
 
     let formData = new FormData(saveComment);
 
-    /* Här använder vi formulärets action attribut istället för ajax variabeln myAjax för URL dit ajax-anroopet ska skickas */
+    /* Here we uses the form's action attribut, instead of the ajax variabel myAjax, for the URL where ajax-request will be sent */
     fetch(saveComment.getAttribute('action'),
     {
         method: 'POST',
